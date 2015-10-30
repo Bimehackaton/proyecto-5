f291d44bb3f2523e199cffd2a5a12a906af42565
unknown <cvirtual@U107666.adm.ehu.es>
2015-10-30 23:43:55 +0100

    map added

--- app/scripts/controllers/car.js
+++ app/scripts/controllers/car.js
@@ -8,12 +8,25 @@
  * Controller of the proyecto5App
  */
 angular.module('proyecto5App')
-  .controller('CarCtrl', function ($scope) {
-  	$scope.map = {
-  	  center: {
-		latitude: 45,
-		longitude: -73
-	  },
-	  zoom: 8
-	};
+  .controller('CarCtrl', function ($scope, $http) {
+    $scope.map = {
+      center: {
+    latitude: 43.266506,
+    longitude: -2.934938
+    },
+    zoom: 8
+  };
+
+  $http({
+    method : 'GET',
+    url    : '/scripts/data/cars.json'
+  }).then(function successCallback(response) {
+    console.log(response);
+      // this callback will be called asynchronously
+      // when the response is available
+    }, function errorCallback(response) {
+      console.error(response);
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
+    });
   });
