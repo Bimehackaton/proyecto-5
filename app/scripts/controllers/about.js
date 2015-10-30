50d600f8ca4f59e650befdb85a623345f04b6b91
unknown <cvirtual@U107666.adm.ehu.es>
2015-10-30 23:10:45 +0100

    esqueleto

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name proyecto5App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the proyecto5App
+ */
+angular.module('proyecto5App')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name proyecto5App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the proyecto5App
+ */
+angular.module('proyecto5App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
